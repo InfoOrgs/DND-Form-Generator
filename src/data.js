4a33e0cd5e0ede68182b21957cddfde6a87b3ee5
InfoOrgs <aditya01377@gmail.com>
2022-09-08 21:20:23 +0530

    implementation

---
+++ src/data.js
@@ -0,0 +1,16 @@
+let InputType = {
+TextInput:`<div class="card inputs-container">
+<input type="text" class="title" name="title" placeholder="Title">
+<input type="text" name="">
+</div>`,
+NumberInput:`<div class="card inputs-container">
+<input type="text" class="title" name="title" placeholder="Title">
+<input type="number" name="">
+</div>`,
+EmailInput:`<div class="card inputs-container">
+<input type="text" class="title" name="title" placeholder="Title">
+<input type="number" name="">
+</div>`
+}
+
+export default InputType ;
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,34 @@
+import InputTypData from '../data.js'
 var items = document.getElementById('main-items')
 new Sortable(items, {
     animation: 330,
     ghostClass: 'blue-background-class'
 });
-console.log('dfdfbvfv')
+let inputContainer = document.getElementById('main-items')
+let inputTypeItem = document.querySelectorAll('.dragable-item')
+inputTypeItem.forEach(e => {
+    e.addEventListener('click', addInputType)
+});
+function addInputType() {
+    let InputType = this.dataset.inputType
+    inputContainer.insertAdjacentHTML( 'beforeend' ,InputTypData[`${InputType}`])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(InputTypData)
