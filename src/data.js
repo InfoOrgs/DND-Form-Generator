63f56c406bacfd93a9c74a153826bca12be569d0
InfoOrgs <aditya01377@gmail.com>
2022-09-10 18:52:25 +0530

    some implements

--- src/data.js
+++ src/data.js
@@ -1,15 +1,21 @@
 let InputType = {
-TextInput:`<div class="card inputs-container">
-<input type="text" class="title" name="title" placeholder="Title">
-<input type="text" name="">
+TextInput:`<div class="card inputs-container animate__animated">
+<span class="material-symbols-outlined gicons">delete</span>
+<input type="text" class="title" name="title" id placeholder="Title">
+<input type="text" name="" placeholder="Text">
+<span class="AdvOpt">Advance Options</span>
 </div>`,
-NumberInput:`<div class="card inputs-container">
-<input type="text" class="title" name="title" placeholder="Title">
-<input type="number" name="">
+NumberInput:`<div class="card inputs-container animate__animated">
+<span class="material-symbols-outlined gicons">delete</span>
+<input type="text" class="title" name="title" id placeholder="Title">
+<input type="text" name="" placeholder="Number">
+<span class="AdvOpt">Advance Options</span>
 </div>`,
-EmailInput:`<div class="card inputs-container">
-<input type="text" class="title" name="title" placeholder="Title">
-<input type="number" name="">
+EmailInput:`<div class="card inputs-container animate__animated">
+<span class="material-symbols-outlined gicons">delete</span>
+<input type="text" class="title" name="title" id placeholder="Title">
+<input type="text" name="" placeholder="Email">
+<span class="AdvOpt">Advance Options</span>
 </div>`
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -4,15 +4,35 @@ new Sortable(items, {
     animation: 330,
     ghostClass: 'blue-background-class'
 });
-let inputContainer = document.getElementById('main-items')
-let inputTypeItem = document.querySelectorAll('.dragable-item')
-inputTypeItem.forEach(e => {
-    e.addEventListener('click', addInputType)
-});
+let inputContainer = document.getElementById('main-items');
+let inputTypeItem = document.querySelectorAll('.dragable-item');
+AddEventListners();
+function AddEventListners() {
+    var DeleteIcons = document.querySelectorAll('.gicons');
+    var AdvanceOpt = document.querySelectorAll('.AdvOpt');
+    inputTypeItem.forEach(e => {
+        e.addEventListener('click', addInputType);
+    }); DeleteIcons.forEach(e => {
+        e.addEventListener('click', DeleteInput);
+    }); AdvanceOpt.forEach(e => {
+        e.addEventListener('click', addAdvanceInfo);
+    });}
 function addInputType() {
     let InputType = this.dataset.inputType
-    inputContainer.insertAdjacentHTML( 'beforeend' ,InputTypData[`${InputType}`])
+    inputContainer.innerHTML += InputTypData[`${InputType}`];
+    AddEventListners();}
+function DeleteInput() {
+    let item = this.parentElement
+    item.classList.add("animate__zoomOut");
+    setTimeout(() => {item.remove()}, 300)
+} function addAdvanceInfo() {
+    console.log('clicked')
 }
+ let logoText =  document.getElementById('logoText')
+let styleColl =['animate__bounce', 'animate__flash','animate__pulse','animate__rubberBand','animate__shakeX','animate__jello','animate__heartBeat']
+let CurrStyle = styleColl[Math.floor(Math.random()*7)]
+ setInterval(() => {logoText.classList.add(CurrStyle)}, 3000)
+
 
 
 
